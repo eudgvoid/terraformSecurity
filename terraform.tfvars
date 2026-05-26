0817fde6c5752333711ce64d98c9c5f5200578bc
Kacper Kalinowski <kacperkalinowski@protonmail.com>
2026-05-26 16:22:28 +0200

    initial commit

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+aws_region   = "us-east-1"
+project_id   = "cmtr-6pajwelx"
+vpc_name = cmtr-6pajwelx-vpc
+
---
+++ variables.tf
@@ -0,0 +1,63 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "project_id" {
+  description = "the project identifier used for tagging"
+  type        = string
+}
+
+variable "vpc_name" {
+  type        = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "vpc_cidr_block" {
+  type = string
+}
+
+variable "public_subnet_name" {
+  type        = string
+}
+
+variable "public_subnet_id" {
+  type = string
+}
+
+variable "private_subnet_name" {
+  type        = string
+}
+
+variable "private_subnet_id" {
+  type = string
+}
+
+variable "public_ec2_instance_name" {
+  type = string
+}
+
+variable "public_ec2_instance_id" {
+  type = string
+}
+
+
+variable "public_ec2_instance_port" {
+  type = string
+}
+
+variable "private_ec2_instance_name" {
+  type = string
+}
+
+variable "private_ec2_instance_id" {
+  type = string
+}
+
+variable "private_ec2_instance_port" {
+  type = string
+}
+
